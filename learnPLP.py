cf23b24a4f9dea34ae52968d30ba9dd49a4e4e84
Okoroego Chijioke Henry <chodasolid@gmail.com>
2025-03-11 01:10:53 -0700

    intro to python

---
+++ learnPLP.py
@@ -0,0 +1,8 @@
+#a,b,c = 5, 7, "Hello world"
+#print(a) # prints 5
+#print(b) # prints 5
+#print(c) # prints Hello World
+
+usersname = input("Please enter your full name: ")
+favColor = input("Please what is your favourite color? ")
+print("Hello,", usersname,"!","your favourite colour is", favColor, "whis is awesome!")
